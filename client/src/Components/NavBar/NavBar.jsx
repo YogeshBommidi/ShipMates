010a8c13a108c405e96e56fbc4c636eedc292fa2
Yogesh <yogeshbommidi@gmail.com>
2024-05-06 17:31:33 +0530

    NavBar completed without responsiveness

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./NavBar.css";
 
 const NavBar = () => {
-  return <div>HEader Section</div>;
+  return (
+    <section className="header-wrapper">
+      <div className="flexCenter paddings innerWidth header-container">
+        <img src="./home-logo.png" alt="Logo" />
+        <ul className="flexCenter orangeText header-menu">
+          <li>MemberShip</li>
+          <li>MarketPlace</li>
+          <li>Transport Directory</li>
+          <button className="btn">LogIn/SignUp</button>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default NavBar;
