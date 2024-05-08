aad070ed32037e5e78d4fd9aa49d02f4d0381717
Yogesh <yogeshbommidi@gmail.com>
2024-05-08 10:36:34 +0530

    deleted to start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
 import "./index.css";
-import Hero from "./Components/Hero/Hero";
 
 const App = () => {
   return (
     <div>
-      <Hero />
+    hello
     </div>
   );
 };
