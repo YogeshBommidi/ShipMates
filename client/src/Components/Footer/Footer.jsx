f0fade9ab3f815d76a03e014e5922eafa050c166
Yogesh <yogeshbommidi@gmail.com>
2024-05-10 11:41:43 +0530

    footer completed

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="paddings innerWidth footer-container">
+        <div className="footer-1">
+          <div className="footer-logo">
+            <img src={"./header-logo.png"} alt="" />
+          </div>
+          <div className="footer-accounts">
+            <ul>
+              <li className="primaryText">Accounts</li>
+              <li className="secondaryText">Privacy Policy</li>
+              <li className="secondaryText">Terms Of Use</li>
+              <li className="secondaryText">Cookie Policy</li>
+              <li className="secondaryText">Careers</li>
+            </ul>
+          </div>
+          <div className="footer-socials">
+            <ul>
+              <li className="primaryText">Socials</li>
+              <li className="secondaryText">Instagram</li>
+              <li className="secondaryText">LinkedIn</li>
+              <li className="secondaryText">Facebook</li>
+              <li className="secondaryText">YouTube</li>
+            </ul>
+          </div>
+        </div>
+        <div className=" paddings footer-2">
+          <div className="copy-right">
+            <span>&copy; Copyright  </span>
+            <a href="">ShipMates</a>
+          </div>
+          <div className="rights">
+          <span>All Right Reserved</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
