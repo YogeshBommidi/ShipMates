36ce8d766f04f209dc529ec587665fcb1f173417
Yogesh <yogeshbommidi@gmail.com>
2024-05-16 15:36:17 +0530

    LoadController completed

--- server/Controllers/LoadControllers.js
+++ server/Controllers/LoadControllers.js
@@ -23,7 +23,32 @@ export const createLoad = asyncHandler(async (req, res) => {
         owner: { connect: { email: userEmail } },
       },
     });
-    res.send({message : "Load Created Successfully", load})
+    res.send({ message: "Load Created Successfully", load });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+export const allLoads = asyncHandler(async (req, res) => {
+  try {
+    const loads = await prisma.Loads.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    res.send(loads);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+export const loadById = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const loadbyid = await prisma.Loads.findUnique({
+      where: { id: id },
+    });
+    res.send(loadbyid);
   } catch (err) {
     throw new Error(err.message);
   }
--- server/Routes/LoadRoutes.js
+++ server/Routes/LoadRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { createLoad } from '../Controllers/LoadControllers.js'
+import { allLoads, createLoad, loadById } from '../Controllers/LoadControllers.js'
 
 const router = express.Router()
 
 router.post("/createLoad", createLoad)
+router.post("/getAllLoads", allLoads)
+router.get("/:id", loadById)
 
 export {router as LoadRoute}
