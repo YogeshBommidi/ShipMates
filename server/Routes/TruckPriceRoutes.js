375866f95f4c73c0b75170050bdd761465935f27
Yogesh <yogeshbommidi@gmail.com>
2024-05-17 17:09:08 +0530

    remved truckPrice router and controller. Updated schema.prisma

--- server/Routes/TruckPriceRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { bidding } from "../Controllers/TruckPriceController.js";
-
-const router = express.Router();
-
-router.post("/bidding", bidding);
-
-export { router as TruckPriceRoute };
