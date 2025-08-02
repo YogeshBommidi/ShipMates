b9a9bf9e53b515fb1d90b4d10aebf174662faaea
Yogesh Bommidi <yogeshbommidi@gmail.com>
2025-08-02 22:34:14 +0530

    baseURL upadated 2.

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://shipmates.vercel.app/api",
+  baseURL: "https://shipmates-server.vercel.app/api",
 });
 
 export const getAllLoads = async () => {
